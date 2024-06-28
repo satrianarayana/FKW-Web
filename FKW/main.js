1ce3e69d56c8e4779e9ef71eacf1272ae8a9b953
Satria Narayana <satrianarayana5@gmail.com>
2024-06-28 17:19:51 +0800

    Better Navbar Mobile, -Image Protection

--- FKW/main.js
+++ FKW/main.js
@@ -1,10 +1,17 @@
 const menuIcon = document.querySelector(".menu-icon");
 const mobileNavMenu = document.querySelector(".mobile-nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
+const imgMenu = document.getElementById("imgMenu")
 
+let toggle = true;
 menuIcon.addEventListener("click", () => {
   mobileNavMenu.classList.toggle("active");
-});
+  toggle = !toggle;
+  if(toggle){
+  imgMenu.src = "https://res.cloudinary.com/dcmo2r3d9/image/upload/v1717737021/menu-icon_pinipn.svg";
+  }else{
+  imgMenu.src = "https://res.cloudinary.com/dcmo2r3d9/image/upload/v1719563465/close-md-svgrepo-com_f6wozy.svg";}
+  });
 
 navLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
@@ -16,7 +23,9 @@ navLinks.forEach((link) => {
       const offset = targetElement.offsetTop - 60;
       window.scrollTo({ top: offset });
     }
-
+    
     mobileNavMenu.classList.remove("active");
+    imgMenu.src = "https://res.cloudinary.com/dcmo2r3d9/image/upload/v1717737021/menu-icon_pinipn.svg";
+    toggle = true;
   });
 });
